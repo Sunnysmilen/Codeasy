58ee0e7fe227ceed73883f355aa5a1422c2f0fac
Luccioni Vincent <vincent.luccioni@gmail.com>
2025-05-21 21:28:15 +0200

    HomePage created, work in progress

---
+++ client/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+function HomePage() {
+  return (
+    <div>
+      <section>
+        <img src="/image-accueil.webp" alt="" />
+        <h1>Bienvenue dans le monde du code ! </h1>
+        <h2>Pas besoin d’être un geek : ici, on apprend en s’amusant.</h2>
+      </section>
+      <button type="button">C'est parti !</button>
+    </div>
+  );
+}
+
+export default HomePage;
