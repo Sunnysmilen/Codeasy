cb7d299dd035c532619710cb8f21eb110ac2e635
Luccioni Vincent <vincent.luccioni@gmail.com>
2025-05-21 21:49:47 +0200

    first css

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,13 @@
+import "./HomePage.css";
+
 function HomePage() {
   return (
-    <div>
+    <div className="homepage-container">
+      <img
+        src="/image-accueil.webp"
+        alt="Robot souhaitant la bienvenue d'un signe de la main"
+      />
       <section>
-        <img src="/image-accueil.webp" alt="" />
         <h1>Bienvenue dans le monde du code ! </h1>
         <h2>Pas besoin d’être un geek : ici, on apprend en s’amusant.</h2>
       </section>
