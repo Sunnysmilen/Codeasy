4a13423f710c04706df9e69ee772f264fd0fc865
sunnysmilen <davidsalnicole@gmail.com>
2025-05-22 12:21:41 +0200

    feat: footer done with mediaqueries

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Outlet />
       <Navbar />
+      <Outlet />
+      <Footer />
     </>
   );
 }
