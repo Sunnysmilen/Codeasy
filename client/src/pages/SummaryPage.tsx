ceffb713d2fcb9b0cc7b6153d0749e893987cd70
MKNG18 <naili.maeva@orange.fr>
2025-05-22 11:07:00 +0200

    feat: added theme component

---
+++ client/src/pages/SummaryPage.tsx
@@ -0,0 +1,12 @@
+import "../assets/styles/SummaryStyle.css";
+import SummaryList from "../components/SummaryList";
+
+function SummaryPage () {
+    return (
+        <div>
+            <SummaryList />
+        </div>
+    )
+}
+
+export default SummaryPage;
