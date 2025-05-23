488f275dabcc3d99285e23f2adaac3b5f4456147
LAU97190 <laury.lutin@gmail.com>
2025-05-23 05:33:58 -0400

    feat: robot

---
+++ client/src/components/Robot.tsx
@@ -0,0 +1,11 @@
+export default function Robot() {
+  return (
+    <div className="robotBloc">
+      <img
+        className="robot"
+        src="/public/giff/welcome.gif"
+        alt="Robot Welcome"
+      />
+    </div>
+  );
+}
