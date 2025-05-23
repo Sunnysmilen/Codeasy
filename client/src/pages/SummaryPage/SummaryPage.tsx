36f92a83da1c4695f5e2a3825d3b92ec078a5266
sunnysmilen <davidsalnicole@gmail.com>
2025-05-23 02:44:05 +0200

    fix: summaryPage style done

--- client/src/pages/SummaryPage/SummaryPage.tsx
+++ client/src/pages/SummaryPage/SummaryPage.tsx
@@ -3,10 +3,10 @@ import "./Card.css";
 
 function SummaryPage() {
   return (
-    <div>
+    <>
       <h1 className="firstTitle">Quelle compétence veux-tu débloquer ?</h1>
       <CardList />
-    </div>
+    </>
   );
 }
 
